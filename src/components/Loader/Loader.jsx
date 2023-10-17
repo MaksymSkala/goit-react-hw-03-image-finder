5ef3fc50eaea012341325df0e12e2f92f683871c
MaksymSkala <maksymskala@gmail.com>
2023-10-17 17:53:28 +0300

    add loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Audio } from 'react-loader-spinner'
-;<Audio
-  height="80"
-  width="80"
-  radius="9"
-  color="green"
-  ariaLabel="loading"
-  wrapperStyle
-  wrapperClass
-/>
+import { Audio } from 'react-loader-spinner';
 
 const Spinner = () => (
   <div className="loader">
-    <Loader type="Puff" color="#00BFFF" height={80} width={80} />
+    <Audio
+      height="80"
+      width="80"
+      radius="9"
+      color="green"
+      ariaLabel="loading"
+      wrapperStyle
+      wrapperClass
+    />
+    ;    
   </div>
 );
 
